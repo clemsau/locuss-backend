fec96c86eb151e18f4fee5882d78d991100687ff
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-06-19 12:27:41 +0200

    Add uuid to card model

--- api/cards/models.py
+++ api/cards/models.py
@@ -1,9 +1,11 @@
+import uuid
 from django.db import models
 
 from api.common.models import BaseModel
 
 
 class Card(BaseModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     front_text = models.CharField(max_length=180)
     back_text = models.CharField(max_length=180)
 
--- api/cards/tests/factories.py
+++ api/cards/tests/factories.py
@@ -6,5 +6,6 @@ class CardFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = 'cards.Card'
 
+    id = factory.Faker('uuid4')
     front_text = factory.Faker('text', max_nb_chars=180)
     back_text = factory.Faker('text', max_nb_chars=180)
