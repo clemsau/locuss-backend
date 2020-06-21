74d389aff423f2b80a777cd1542db1ba318ddf1f
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-06-21 13:31:29 +0200

    Fix deck factory

--- api/decks/tests/test_models.py
+++ api/decks/tests/test_models.py
@@ -1,13 +1,11 @@
 from django.test import TestCase
 
 from api.decks.tests.factories import DeckFactory
-from api.users.tests.factories import UserFactory
 
 
 class TestDeckModel(TestCase):
     def setUp(self):
-        self.user = UserFactory()
-        self.deck = DeckFactory(owner=self.user)
+        self.deck = DeckFactory()
 
     def test_deck_str(self):
         """
