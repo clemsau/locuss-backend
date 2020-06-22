0832b31f68ef017301599c8c019ffa5fd5a358ef
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-06-22 11:37:06 +0200

    Add redoc generation

--- api/urls.py
+++ api/urls.py
@@ -3,16 +3,34 @@ from django.urls import path, re_path, include, reverse_lazy
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic.base import RedirectView
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from .users.views import UserViewSet, UserCreateViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'users', UserCreateViewSet)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Locuss API redoc",
+      default_version='v1',
+      description="Locuss API documentation",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="admin@example.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/v1/', include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
