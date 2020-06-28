259c5cf8068e8dcf29a38055785aa2f1cd95397c
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-06-28 03:19:01 +0200

    Modify Objects permissions

--- api/decks/views.py
+++ api/decks/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets
+from .permissions import IsDeckOwnerOrContributor
+
+from api.common.permissions import ReadOnly
 from .models import Deck
 from .serializers import DeckSerializer
 
 
 class ListDeckView(viewsets.ModelViewSet):
+    permission_classes = [IsDeckOwnerOrContributor | ReadOnly]
     queryset = Deck.objects.all()
     serializer_class = DeckSerializer
