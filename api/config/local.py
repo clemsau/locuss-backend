87a90cd8ad4898bff30eb171b61e2939727fac88
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-07-26 11:32:33 +0200

    Add django-extensions to the local requirements

--- api/config/local.py
+++ api/config/local.py
@@ -8,7 +8,8 @@ class Local(Common):
 
     # Testing
     INSTALLED_APPS = Common.INSTALLED_APPS
-    INSTALLED_APPS += ('django_nose',)
+    INSTALLED_APPS += ('django_nose',
+                       'django_extensions',)
     TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
     NOSE_ARGS = [
         BASE_DIR,
