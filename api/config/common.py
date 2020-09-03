1b40a320b195624bd7b421a1b393919546ce4bb8
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-09-03 21:00:21 +0200

    Move django-extensions to the common settings

--- api/config/common.py
+++ api/config/common.py
@@ -21,6 +21,7 @@ class Common(Configuration):
         'rest_framework',            # utilities for rest apis
         'rest_framework.authtoken',  # token authentication
         'django_filters',            # for filtering rest endpoints
+        'django_extensions',         # for additional django features
         'drf_yasg',                  # for api documentation generation
 
         # Your apps
